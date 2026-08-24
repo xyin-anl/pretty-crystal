44e0cc1431c83d7087053d3ce1e40e8d5ff39f51
Xiangyu Yin <xyin@anl.gov>
2026-08-23 20:43:08 -0500

    build: refresh bundled web renderer

--- src/pretty_crystal/web_static/assets/index-BQRR4MPo.js
+++ src/pretty_crystal/web_static/assets/index-BQRR4MPo.js
@@ -1,4 +1,4 @@
-import{p as Ur}from"./index-CHecS04q.js";import"./index-B_3Meemo.js";/*! *****************************************************************************
+import{p as Ur}from"./index-CQg2fXNL.js";import"./index-AxEtRNPZ.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
